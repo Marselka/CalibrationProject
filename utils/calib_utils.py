26e5f97c7675b77eaabbbf124866eaf96c9a1787
Konstantin <ankalagon_black@mail.ru>
2021-03-25 11:49:35 +0300

    New azure-s10 extrinsics calibration

--- utils/calib_utils.py
+++ utils/calib_utils.py
@@ -1,5 +1,8 @@
 import cv2 as cv
 import numpy as np
+from scipy.interpolate import griddata
+from scipy.linalg import svd
+from scipy.optimize import least_squares
 
 
 def detect_keypoints(images, pattern_size, edge_length=1.0):
@@ -14,7 +17,7 @@ def detect_keypoints(images, pattern_size, edge_length=1.0):
     scene_points = np.zeros((num_points, 3), np.float32)
     scene_points[:, :2] = np.mgrid[0:pattern_size[0], 0:pattern_size[1]].T.reshape(-1, 2) * edge_length
 
-    results = {}
+    detections = {}
 
     for key, img in images.items():
         if img.shape[-1] == 3:
@@ -31,94 +34,356 @@ def detect_keypoints(images, pattern_size, edge_length=1.0):
         if success:
             loc_kp = cv.cornerSubPix(gray_img, kp, (11, 11), (-1, -1), criteria)
 
-            results[key] = (scene_points, loc_kp)
+            detections[key] = (scene_points, loc_kp)
 
-    return results
-
-
-def filter_orientation(results1, pattern_size):
-    fresults = {}
-
-    keys = list(results1.keys())
-    anchor_kp = results1[keys[0]][1]
-
-    anchor_is_bottom = (anchor_kp[pattern_size[0]] - anchor_kp[0])[0, 1] > 0
-    anchor_is_right = (anchor_kp[1] - anchor_kp[0])[0, 0] > 0
-
-    for k in keys[1:]:
-        r = results1[k]
-        sp, loc_kp = r[0], r[1]
-
-        is_bottom = (loc_kp[pattern_size[0]] - loc_kp[0])[0, 1] > 0
-        is_right = (loc_kp[1] - loc_kp[0])[0, 0] > 0
-
-        if anchor_is_bottom != is_bottom:
-            sp = np.flip(sp, axis=0)
-
-        if anchor_is_right != is_right:
-            sp = np.flip(sp, axis=1)
-
-        fresults[k] = (sp, loc_kp)
-
-    return fresults
+    return detections
 
 
-def filter_orientation_stereo(results1, results2, pattern_size):
-    fresults1 = filter_orientation(results1, pattern_size)
-    fresults2 = filter_orientation(results2, pattern_size)
+def undistort_intrinsics(images, intrinsics, dist_coeff):
+    shape = next(iter(images.values())).shape[::-1][1:]
+    undist_intrinsics, _ = cv.getOptimalNewCameraMatrix(intrinsics, dist_coeff, shape, 1, shape)
 
-    return fresults1, fresults2
-
-
-def get_calib_input(results):
-    scene_points = []
-    loc_kp = []
-
-    for key1, r1 in results.items():
-        scene_points.append(r1[0])
-        loc_kp.append(r1[1])
-
-    return scene_points, loc_kp
+    return undist_intrinsics
 
 
-def get_stereo_calib_input(fresults1, fresults2, pair_dict):
-    scene_points = []
-    loc_kp1 = []
-    loc_kp2 = []
+def undistort_images(images, intrinsics, dist_coeff, undist_intrinsics):
+    undist_images = {}
 
-    for key1, r1 in fresults1.items():
-        key2 = pair_dict[key1]
-        r2 = fresults2[key2]
+    for key, img in images.items():
+        undist_img = cv.undistort(img, intrinsics, dist_coeff, None, undist_intrinsics)
 
-        scene_points.append(r1[0])
-        loc_kp1.append(r1[1])
-        loc_kp2.append(r2[1])
+        undist_images[key] = undist_img
 
-    return scene_points, loc_kp1, loc_kp2
+    return undist_images
 
 
-# def check_neigh_consistency(results, pattern_size, const_thresh=3.0):
-#     """
-#     The function expects a list of images with moderate (or small) latency between adjacent
-#     items by default. It's needed in order to find movement in the sequence and eliminate corresponding images
-#     as it will cause problems when calculating [R,t] between stereo cameras.
-#     :param const_thresh: The maximum displacement between corresponding keypoints on neighbouring images
-#     """
-#     num_points = pattern_size[0] * pattern_size[1]
-#
-#     results = dict(results)
-#
-#     keys = list(results.keys())
-#
-#     prev_loc_kp = results[keys[0]][1]
-#
-#     for k in keys[1:]:
-#         curr_loc_kp = results[k][1]
-#         diff_norm = np.linalg.norm((curr_loc_kp - prev_loc_kp).reshape(num_points, 2), axis=-1).mean()
-#
-#         prev_loc_kp = curr_loc_kp
-#
-#         if diff_norm > const_thresh:
-#             del results[k]
-#
-#     return results
+def undistort_depths(depths, intrinsics, dist_coeff, undist_intrinsics):
+    undist_depths = {}
+
+    for key, depth in depths.items():
+        map_x, map_y = cv.initUndistortRectifyMap(intrinsics, dist_coeff, None, undist_intrinsics, depth.shape[::-1], cv.CV_32FC1)
+        undist_depth = cv.remap(depth, map_x, map_y, cv.INTER_NEAREST)
+
+        undist_depths[key] = undist_depth
+
+    return undist_depths
+
+
+def pointcloudify_depths(depths, undist_intrinsics):
+    pcd_depths = {}
+
+    for key, depth in depths.items():
+        shape = depth.shape[::-1]
+
+        grid_x, grid_y = np.meshgrid(np.arange(shape[0]), np.arange(shape[1]))
+        grid = np.concatenate([np.expand_dims(grid_x, -1),
+                               np.expand_dims(grid_y, -1)], axis=-1)
+
+        norm_grid = to_norm_image_coord(grid.reshape(-1, 2), undist_intrinsics)
+
+        # Raise by undistorted depth value from image plane to local camera space
+        local_grid = norm_grid * np.expand_dims(depth.reshape(-1), axis=-1)
+
+        pcd_depths[key] = local_grid.astype(np.float32)
+
+    return pcd_depths
+
+
+def to_norm_image_coord(loc_kp, intrinsics):
+    """
+    :param loc_kp: N x 2
+    :param intrinsics: 3 x 3
+    """
+    return (np.linalg.inv(intrinsics) @ to_homogeneous(loc_kp).transpose()).transpose()
+
+
+def project2image(scene_points, intrinsics):
+    """
+    :param scene_points: N x 3
+    :param intrinsics: 3 x 3
+    """
+    return to_cartesian((intrinsics @ scene_points.transpose()).transpose())
+
+
+def transform2local(loc_kp, norm_loc_kp, depth):
+    """
+    :param loc_kp: N x 2
+    :param norm_loc_kp: N x 2
+    :param depth: H x W
+    """
+    loc_kp_depth = interpolate(depth, loc_kp)
+
+    local_loc_kp = norm_loc_kp * np.expand_dims(loc_kp_depth.reshape(-1), axis=-1)
+
+    return local_loc_kp
+
+
+def plane2plane_transformation(plane_points1, plane_points2):
+    """
+    :param plane_points1: N x 3
+    :param plane_points2: N x 3
+    """
+    centroid1, centroid2 = plane_points1.mean(axis=0), plane_points2.mean(axis=0)
+
+    plane_points1 = plane_points1 - centroid1.reshape(1, 3)
+    plane_points2 = plane_points2 - centroid2.reshape(1, 3)
+
+    H = plane_points2.T.dot(plane_points1).T
+    u, d, vt = svd(H, full_matrices=False)
+
+    # If a singular value is close to zero then the general solution will not hold
+    num_sing = sum(d < 1e-10)
+    if num_sing == 1:
+        w, v = np.linalg.eig(H.T @ H)
+
+        sing_mask = w > 1e-10
+        s = sum([np.outer(vi, vi) / np.sqrt(wi) for wi, vi in zip(w[sing_mask], v[:, sing_mask].T)])
+        s3 = np.outer(v[~sing_mask], v[~sing_mask])
+
+        R = H.T @ s + s3
+
+        if np.linalg.det(R) < 0:
+            R = H.T @ s - s3
+
+    elif num_sing == 0:
+        R = u @ vt
+
+    else:
+        raise RuntimeError
+
+    T = np.zeros((4, 4))
+    T[:3, :3] = R
+    T[:3, 3] = centroid2 - R @ centroid1
+    T[3, 3] = 1
+
+    return T
+
+
+def compose_fund_mat(T, K1, K2):
+    E = compose_ess_mat(T)
+
+    iK1, iK2 = np.linalg.inv(K1), np.linalg.inv(K2)
+
+    return iK2.T @ E @ iK1
+
+
+def optimize_pose_lm(T, loc_kp1, loc_kp2, K1, K2):
+    """
+    :param T: 4 x 4
+    :param loc_kp1: N x 2
+    :param loc_kp2: N x 2
+    :param K1: 3 x 3
+    :param K2: 3 x 3
+    """
+    T = T.copy()
+    dist = np.linalg.norm(T[:3, 3])
+    T[:3, 3] /= dist
+
+    x = encode_essmat(T)
+
+    iK1, iK2 = np.linalg.inv(K1), np.linalg.inv(K2)
+
+    lm_opt = least_squares(loss_fun, x, jac=loss_fun_jac, args=(loc_kp1, loc_kp2, iK1, iK2), method='lm')
+
+    if lm_opt.success:
+        print("Avg. epipolar distance: ", lm_opt.cost)
+
+        T = np.zeros((4, 4))
+        T[:3, :3] = cv.Rodrigues(lm_opt.x[:3])[0]
+
+        t = np.zeros(3)
+        t[0:2] = lm_opt.x[3:]
+        t = cv.Rodrigues(t)[0][:, 2]
+        T[:3, 3] = t * dist
+
+        T[3, 3] = 1
+
+        return T
+
+    else:
+        return None
+
+
+"""
+Utils
+"""
+
+
+def interpolate(grid_values, points):
+    grid_x, grid_y = np.meshgrid(np.arange(grid_values.shape[1]), np.arange(grid_values.shape[0]))
+    grid = np.concatenate([np.expand_dims(grid_x, -1),
+                           np.expand_dims(grid_y, -1)], axis=-1)
+
+    return griddata(grid.reshape(-1, 2), grid_values.reshape(-1), points, method='nearest', fill_value=0)
+
+
+def to_homogeneous(t):
+    return np.concatenate((t, np.ones((len(t), 1))), axis=-1)
+
+
+def to_cartesian(t):
+    return t[:, :-1] / np.expand_dims(t[:, -1], -1)
+
+
+def vec_to_cross(x):
+    return np.array([[0, -x[2], x[1]],
+                     [x[2], 0, -x[0]],
+                     [-x[1], x[0], 0]])
+
+
+def compose_ess_mat(T):
+    R, t = T[:3, :3], T[:3, 3]
+
+    cp_t = vec_to_cross(t)
+    E = cp_t @ R
+
+    return E
+
+
+def loss_fun(x, kp1, kp2, iK1, iK2):
+    E = decode_essmat(x)
+    F = iK2.T @ E @ iK1
+    errs, errs_rev = compute_epipolar_errors_opt(F, kp1, kp2)
+    return np.concatenate([errs, errs_rev], axis=0)
+
+
+def loss_fun_jac(x, kp1, kp2, iK1, iK2):
+    E, dE = decode_essmat(x, True)
+    F = iK2.T @ E @ iK1
+    dF = np.matmul(iK2.T.reshape(1, 3, 3), np.matmul(dE, iK1.reshape(1, 3, 3)))
+    _, _, derrs, derrs_rev = compute_epipolar_errors_opt(F, kp1, kp2, dF)
+    dres = np.concatenate([derrs.T, derrs_rev.T], axis=0)
+    return dres
+
+
+def compute_epipolar_errors_opt(F, kp1, kp2, dF=None):
+    n = kp1.shape[0]
+    kp1h = np.concatenate([kp1, np.ones((n, 1))], axis=1)
+    kp2h = np.concatenate([kp2, np.ones((n, 1))], axis=1)
+    luh = kp1h @ F.T
+    lambdas = np.linalg.norm(luh[:, 0:2], axis=1).reshape(n, 1)
+    lh = luh / lambdas
+    errs = np.matmul(lh.reshape(n, 1, 3), kp2h.reshape(n, 3, 1)).reshape(-1)
+
+    luh_rev = kp2h @ F
+    lambdas_rev = np.linalg.norm(luh_rev[:, 0:2], axis=1).reshape(n, 1)
+    lh_rev = luh_rev / lambdas_rev
+    errs_rev = np.matmul(lh_rev.reshape(n, 1, 3), kp1h.reshape(n, 3, 1)).reshape(-1)
+    if dF is None:
+        return errs, errs_rev
+
+    dFt = dF.transpose(0, 2, 1)
+    derrs = diff_ptl_errs(kp1h, kp2h, luh, lambdas, dFt)
+    derrs_rev = diff_ptl_errs(kp2h, kp1h, luh_rev, lambdas_rev, dF)
+
+    return errs, errs_rev, derrs, derrs_rev
+
+
+def diff_ptl_errs(kp1h, kp2h, luh, lambdas, dFt, F=None, lh=None):
+    n = kp1h.shape[0]
+    dluh = np.matmul(kp1h.reshape(1, n, 3), dFt)  # 5 x n x 3
+    dlnh = (1.0 / lambdas).reshape(n, 1, 1) * np.eye(3).reshape(1, 3, 3)  # n, 3, 3
+    lzuh = np.concatenate([luh[:, 0:2], np.zeros((n, 1))], axis=1)
+    dlnh -= (lambdas ** (-3)).reshape(n, 1, 1) * np.matmul(luh.reshape(n, 3, 1), lzuh.reshape(n, 1, 3))
+    dlh = np.matmul(dlnh.reshape(1, n, 3, 3), dluh.reshape(5, n, 3, 1)).reshape(5, n, 3)
+    derrs = np.matmul(dlh.reshape(5, n, 1, 3), kp2h.reshape(1, n, 3, 1)).reshape(5, n)
+    if F is None:
+        return derrs
+    derr_pt = diff_ptl_errs_pts(kp1h, kp2h, lh, dlh, dlnh, F, dFt, lambdas, luh, lzuh, dluh)
+    return derrs, derr_pt
+
+
+def diff_ptl_errs_pts(kp1h, kp2h, lh, dlh, dlnh, F, dFt, lambdas, luh, lzuh, dluh):
+    n = kp1h.shape[0]
+
+    derr2 = lh.transpose()[0:2, :]  # 3 x n
+    d2err2 = dlh.reshape(5, n, 3).transpose(0, 2, 1)[:, 0:2, :]  # 5 x 3 x n
+
+    derr = np.matmul(kp2h.reshape(n, 1, 3), np.matmul(dlnh.reshape(n, 3, 3),
+                                                      F.reshape(1, 3, 3))).reshape(n, 3)[:, 0:2].T
+    # d2err = np.matmul(kp2h.reshape(1, n, 1, 3),
+    #                   np.matmul(dlnh.reshape(1, n, 3, 3),
+    #                   dFt.reshape(5, 1, 3, 3).transpose(0, 1, 3, 2))).reshape(5, n, 3)[:, :, 0:2].transpose(0, 2, 1)
+    luh_dx = F.transpose().reshape(1, 3, 3)
+    dlambda = np.matmul(luh_dx, lzuh.reshape(n, 3, 1))  # n x 3 x 1
+    d = np.eye(3).reshape(1, 1, 3, 3) * dlambda.reshape(n, 3, 1, 1) * (
+        -lambdas.reshape(n, 1, 1, 1) ** (-3))  # n x 3 x 3 x 3
+    d += 3 * lambdas.reshape(n, 1, 1, 1) ** (-5) * np.matmul(luh.reshape(n, 1, 3, 1),
+                                                             lzuh.reshape(n, 1, 1, 3)) * dlambda.reshape(n, 3, 1, 1)
+
+    d += -(lambdas ** (-3)).reshape(n, 1, 1, 1) * np.matmul(luh_dx.reshape(1, 3, 3, 1), lzuh.reshape(n, 1, 1, 3))
+    F0 = np.copy(F)
+    F0[2, :] = 0
+    luh0_dx = F0.transpose().reshape(1, 3, 1, 3)
+    d += -(lambdas ** (-3)).reshape(n, 1, 1, 1) * np.matmul(luh.reshape(n, 1, 3, 1), luh0_dx)
+
+    # dluh: 5 x n x 3
+    dmult = np.matmul(d.reshape(1, n, 3, 3, 3), dluh.reshape(5, n, 1, 3, 1)).reshape(5, n, 3, 3, 1)
+    dmult += np.matmul(dlnh.reshape(1, n, 1, 3, 3), dFt.reshape(5, 1, 3, 3, 1)).reshape(5, n, 3, 3, 1)  # old
+
+    d2err = np.matmul(kp2h.reshape(1, n, 1, 1, 3), dmult).reshape(5, n, 3)[:, :, 0:2].transpose(0, 2, 1)
+
+    return derr, d2err, derr2, d2err2
+
+
+def encode_essmat(T):
+    R, t = T[:3, :3], T[:3, 3]
+
+    R_param, jac = cv.Rodrigues(R)
+
+    vec = np.asarray([0, 0, 1])
+    t_param = rotate_a_b_axis_angle(vec, t)
+
+    x = np.concatenate([R_param.reshape(-1), t_param[0:2]], axis=0)
+
+    return x
+
+
+def decode_essmat(x, is_jac=False):
+    # assuming jac is 3 x 9
+    R, jac = cv.Rodrigues(x[0:3])
+    x2 = np.zeros(3)
+    x2[0:2] = x[3:5]
+    R2, jac2 = cv.Rodrigues(x2)
+    t = R2[:, 2]
+    cp_t = vec_to_cross(t)
+    E = cp_t @ R
+    if not is_jac:
+        return E
+
+    jac_big = np.zeros((5, 3, 3))
+    jac_big[0:3, :, :] = np.matmul(cp_t.reshape(1, 3, 3), jac.reshape(3, 3, 3))
+
+    dcpt1 = np.zeros((3, 3))
+    dcpt1[1, 2] = -1
+    dcpt1[2, 1] = 1
+    jac_big[3:5, :, :] = (dcpt1 @ R).reshape(1, 3, 3) * jac2[0:2, 2].reshape(2, 1, 1)
+
+    dcpt2 = np.zeros((3, 3))
+    dcpt2[0, 2] = 1
+    dcpt2[2, 0] = -1
+    jac_big[3:5, :, :] += (dcpt2 @ R).reshape(1, 3, 3) * jac2[0:2, 5].reshape(2, 1, 1)
+
+    dcpt3 = np.zeros((3, 3))
+    dcpt3[0, 1] = -1
+    dcpt3[1, 0] = 1
+    jac_big[3:5, :, :] += (dcpt3 @ R).reshape(1, 3, 3) * jac2[0:2, 8].reshape(2, 1, 1)
+
+    return E, jac_big
+
+
+def rotate_a_b_axis_angle(a, b):
+    a = a / np.clip(np.linalg.norm(a), a_min=1e-16, a_max=None)
+    b = b / np.clip(np.linalg.norm(b), a_min=1e-16, a_max=None)
+    rot_axis = np.cross(a, b)
+    #   find a proj onto b
+    a_proj = b * (a.dot(b))
+    a_ort = a - a_proj
+    #   find angle between a and b in [0, np.pi)
+    theta = np.arctan2(np.linalg.norm(a_ort), np.linalg.norm(a_proj))
+    if a.dot(b) < 0:
+        theta = np.pi - theta
+
+    aa = rot_axis / np.clip(np.linalg.norm(rot_axis), a_min=1e-16, a_max=None) * theta
+    return aa
+
--- utils/io_utils.py
+++ utils/io_utils.py
@@ -1,8 +1,28 @@
 import os
+import re
 import open3d
 import cv2 as cv
 import numpy as np
-import matplotlib.pyplot as plt
+
+
+def get_matching_pairs(folder_path1, folder_path2, file_filter='\d{5}.*\.(png|jpg|jpeg)$'):
+    file_names1, file_names2 = sorted(filter_files(os.listdir(folder_path1), file_filter), key=str.lower), \
+                               sorted(filter_files(os.listdir(folder_path2), file_filter), key=str.lower)
+
+    timestamps1, timestamps2 = np.array([int(fn.split('.')[0]) for fn in file_names1]), \
+                               np.array([int(fn.split('.')[0]) for fn in file_names2])
+
+    diff = np.abs(timestamps1.reshape(-1, 1) - timestamps2.reshape(1, -1))
+    nn_indices1 = np.argmin(diff, axis=-1)
+    nn_indices2 = np.argmin(diff, axis=-2)
+
+    mnn_mask = nn_indices2[nn_indices1] == np.arange(len(nn_indices1))
+
+    timestamps1 = timestamps1[mnn_mask]
+    timestamps2 = timestamps2[nn_indices1][mnn_mask]
+
+    return timestamps1.astype(np.str), timestamps2.astype(np.str)
+
 
 """
 Functions for reading the data
@@ -28,7 +48,7 @@ def get_pointclouds(folder_path, start=None, period=None, file_name_list=None):
     return get_data(load_pcd, folder_path, start, period, file_name_list)
 
 
-def get_depth(folder_path, start=None, period=None, file_name_list=None):
+def get_depths(folder_path, start=None, period=None, file_name_list=None):
     """
     :param folder_path: path to the folder with depths
     :param start: depth to start from
@@ -66,7 +86,10 @@ def get_data(data_loader, folder_path, start, period, file_name_list):
                 data[fname] = datai
 
     else:
-        for fname in file_name_list:
+        for i, fname in enumerate(file_name_list):
+            if period is not None and i % period != 0:
+                continue
+
             fpath = os.path.join(folder_path, fname)
 
             datai = data_loader(fpath)
@@ -77,198 +100,29 @@ def get_data(data_loader, folder_path, start, period, file_name_list):
     return data
 
 
-def pointcloudify_depths(depths, intrinsics, dist_coeff, undistort=True):
-    pointclouds = {}
-
-    shape = list(depths.values())[0].shape[::-1]
-
-    # Calculate undistorted intrinsics
-    if undistort:
-        undist_intrinsics, _ = cv.getOptimalNewCameraMatrix(intrinsics, dist_coeff, shape, 1, shape)
-        inv_undist_intrinsics = np.linalg.inv(undist_intrinsics)
-
-    else:
-        inv_undist_intrinsics = np.linalg.inv(intrinsics)
-
-    for key, depthi in depths.items():
-        # Undistort depth
-        if undistort:
-            # undist_depthi = cv.undistort(depthi, intrinsics, dist_coeff, None, undist_intrinsics)
-            map_x, map_y = cv.initUndistortRectifyMap(intrinsics, dist_coeff, None, undist_intrinsics, shape, cv.CV_32FC1)
-            undist_depthi = cv.remap(depthi, map_x, map_y, cv.INTER_NEAREST)
-
-        # Generate x,y grid for H x W image
-        grid_x, grid_y = np.meshgrid(np.arange(shape[0]), np.arange(shape[1]))
-        grid = np.concatenate([np.expand_dims(grid_x, -1),
-                               np.expand_dims(grid_y, -1)], axis=-1)
-
-        grid = np.concatenate([grid, np.ones((shape[1], shape[0], 1))], axis=-1)
-
-        # To normalized image coordinates
-        local_grid = inv_undist_intrinsics @ grid.reshape(-1, 3).transpose()  # 3 x H * W
-
-        # Raise by undistorted depth value from image plane to local camera space
-        if undistort:
-            local_grid = local_grid.transpose() * np.expand_dims(undist_depthi.reshape(-1), axis=-1)
-
-        else:
-            local_grid = local_grid.transpose() * np.expand_dims(depthi.reshape(-1), axis=-1)
-
-        pointclouds[key] = local_grid.astype(np.float32)
-
-    return pointclouds
-
-
 """
-Functions for writing the data and providing visualizations
+Functions for saving the data
 """
 
 
-def output_calib_results(intrinsics, dist_coeff, shape, images, idx, calib_name=None):
-    print("Intrinsics:")
-    print(intrinsics)
+def save_intrinsics_calib(calib_name, intrinsics, dist_coeff, undist_intrinsics):
+    calib_fpath = 'calib_output/%s_intrinsics' % calib_name
 
-    print("Distortion coefficients:")
-    print(dist_coeff)
+    calib = {'intrinsics': intrinsics,
+             'dist_coeff': dist_coeff,
+             'undist_intrinsics': undist_intrinsics}
 
-    undist_intrinsics, _ = cv.getOptimalNewCameraMatrix(intrinsics, dist_coeff, shape, 1, shape)
+    np.save(calib_fpath, calib)
 
-    print("Undistored intrinsics: ")
-    print(undist_intrinsics)
-
-    undist_img = cv.undistort(images[list(images.keys())[idx]], intrinsics, dist_coeff, None, undist_intrinsics)
-
-    plt.figure(figsize=(9, 9))
-    plt.imshow(undist_img)
-
-    if calib_name is not None:
-        calib_fpath = 'calib_output/%s_intrinsics' % calib_name
-        calib = {'intrinsics': intrinsics,
-                 'dist_coeff': dist_coeff,
-                 'undist_intrinsics': undist_intrinsics}
-
-        np.save(calib_fpath, calib)
-
-        print('Saved calibration results as %s.npy' % calib_fpath)
+    print('Saved calibration results as %s.npy' % calib_fpath)
 
 
-def output_stereo_calib_results(R, T, E, F, images1, images2, results1, results2, pattern_size, idx, pair_dict,
-                                calib_name=None):
-    print("R:")
-    print(R)
+def save_extrinsics_calib(calib_name, T):
+    calib_fpath = 'calib_output/%s_extrinsics' % calib_name
+    calib = {'T': T}
 
-    print("T:")
-    print(T)
-
-    print("E:")
-    print(E)
-
-    print("F:")
-    print(F)
-
-    num_points = pattern_size[0] * pattern_size[1]
-
-    key1 = list(images1.keys())[idx]
-    key2 = pair_dict[key1]
-
-    res1 = results1.get(key1)
-    res2 = results2.get(key2)
-
-    if res1 is not None and res2 is not None:
-        fig, axes = plt.subplots(2, 2, figsize=(18, 12))
-
-        loc_kp1 = res1[1].reshape(num_points, 1, 2)
-        loc_kp2 = res2[1].reshape(num_points, 1, 2)
-
-        ep_lines0 = cv.computeCorrespondEpilines(loc_kp2, 2, F).reshape(-1, 3)
-        ep_lines1 = cv.computeCorrespondEpilines(loc_kp1, 1, F).reshape(-1, 3)
-
-        ep_img0 = draw_ep_lines(np.copy(images1[key1]), ep_lines0, loc_kp1)
-        det_img1 = cv.drawChessboardCorners(np.copy(images2[key2]), pattern_size, loc_kp2, True)
-
-        det_img0 = cv.drawChessboardCorners(np.copy(images1[key1]), pattern_size, loc_kp1, True)
-        ep_img1 = draw_ep_lines(np.copy(images2[key2]), ep_lines1, loc_kp2)
-
-        axes[0][0].imshow(ep_img0)
-        axes[0][1].imshow(det_img1)
-
-        axes[1][0].imshow(det_img0)
-        axes[1][1].imshow(ep_img1)
-
-    else:
-        print("The pair doesn't have matching detections")
-
-    if calib_name is not None:
-        calib_fpath = 'calib_output/%s_extrinsics' % calib_name
-        calib = {'R': R,
-                 'T': T,
-                 'E': E,
-                 'F': F}
-
-        np.save(calib_fpath, calib)
-
-        print('Saved calibration results as %s.npy' % calib_fpath)
-
-
-"""
-Functions for visualization only
-"""
-
-
-def draw_detections(images, results, pattern_size, idx, normalize=False):
-    key = list(results.keys())[idx]
-    res = results.get(key)
-
-    if res is not None:
-        det_img = np.copy(images[key]).astype(np.float32)
-
-        if normalize:
-            det_img = det_img / det_img.reshape(-1, 3).max(axis=0).reshape(1, 1, 3)
-
-        det_img = cv.drawChessboardCorners(det_img, pattern_size, res[1], True)
-
-        plt.figure(figsize=(9, 9))
-        plt.imshow(det_img)
-
-    else:
-        print("No detections")
-
-
-def draw_stereo_pair(images1, images2, idx, pair_dict):
-    key1 = list(images1.keys())[idx]
-    key2 = pair_dict[key1]
-
-    img1 = images1.get(key1)
-    img2 = images2.get(key2)
-
-    if img1 is not None and img2 is not None:
-        fig, axes = plt.subplots(1, 2, figsize=(18, 7))
-
-        axes[0].imshow(img1)
-        axes[1].imshow(img2)
-
-    else:
-        print("Stereo pair doesn't exist")
-
-
-def draw_stereo_pair_detections(images1, images2, results1, results2, pattern_size, idx, pair_dict):
-    key1 = list(images1.keys())[idx]
-    key2 = pair_dict[key1]
-
-    res1 = results1.get(key1)
-    res2 = results2.get(key2)
-
-    if res1 is not None and res2 is not None:
-        fig, axes = plt.subplots(1, 2, figsize=(18, 7))
-
-        det_img1 = cv.drawChessboardCorners(np.copy(images1.get(key1)), pattern_size, res1[1], True)
-        det_img2 = cv.drawChessboardCorners(np.copy(images2.get(key2)), pattern_size, res2[1], True)
-
-        axes[0].imshow(det_img1)
-        axes[1].imshow(det_img2)
-
-    else:
-        print("The pair doesn't have matching detections")
+    np.save(calib_fpath, calib)
+    print('Saved calibration results as %s.npy' % calib_fpath)
 
 
 """
@@ -308,16 +162,5 @@ def load_depth(file_path):
     return pcd
 
 
-def draw_ep_lines(img, ep_line, loc_kp):
-    c = img.shape[1]
-
-    for l, lk in zip(ep_line, loc_kp):
-        color = tuple(np.random.randint(0, 255, 3).tolist())
-
-        x0, y0 = map(int, [0, -l[2] / l[1]])
-        x1, y1 = map(int, [c, -(l[2] + l[0] * c) / l[1]])
-
-        img = cv.line(img, (x0, y0), (x1, y1), color, 1, lineType=cv.LINE_AA)
-        img = cv.circle(img, tuple(lk[0]), 5, color, -1)
-
-    return img
+def filter_files(file_names, file_filter):
+    return [fn for fn in file_names if re.match(file_filter, fn)]
